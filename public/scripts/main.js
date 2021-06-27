78a3bb924678e14c4265f0da5968e891000f4dc3
Deivid Souza <ddeividd.eu@hotmail.com>
2021-06-27 17:46:55 -0300

    Adicionar rotas. Incrementar o Modal

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,22 +2,44 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 // Pegar todos os botões que exitem com a classe check
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button => {
     // adicionar a escuta
-    button.addEventListener("click", event => {
-        // Abrir modal
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 })
 
 // Quando o botão delete for clicado, ele abre a modal
 const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button => {
-    button.addEventListener("click", event => {
-        modal.open()
-    })
+    button.addEventListener("click", (event) => handleClick (event, false))
 })
+
+function handleClick(event, check = true){     
+
+    // para não aparecer jogo da velha na url quando o botão for clicado
+    event.preventDefault()
+    
+    const text = check ? "Marcar como lida" : "Excluir"
+    const slug = check ? "check" : "delete"
+    const roomId = document.querySelector("#room-id").dataset.id
+    const questionId = event.target.dataset.id
+
+    const form = document.querySelector(".modal form")
+    form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`)
+
+    modalTitle.innerHTML = `${text} esta pergunta`
+    modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?`
+    modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+
+    check ? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+    // abrir modal
+    modal.open()
+}
